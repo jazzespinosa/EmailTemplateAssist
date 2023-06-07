8c2f986c6b0f9bed22392c40d3d4eeab1c4120e5
jazzespinosa <jazel.espinosa96@gmail.com>
2023-06-08 02:06:36 +0800

    initial creation - for APAC version

--- mainForm.Designer.cs
+++ mainForm.Designer.cs
@@ -33,34 +33,40 @@
             this.cbxTemplate = new System.Windows.Forms.ComboBox();
             this.btnSend = new System.Windows.Forms.Button();
             this.tbxUsername = new System.Windows.Forms.TextBox();
+            this.cboxSubTemplate = new System.Windows.Forms.ComboBox();
+            this.labelSubTemplate = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Location = new System.Drawing.Point(8, 56);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 19);
+            this.label1.Size = new System.Drawing.Size(88, 19);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Template:";
+            this.label1.Text = "Application:";
             // 
             // cbxTemplate
             // 
+            this.cbxTemplate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.cbxTemplate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbxTemplate.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbxTemplate.Font = new System.Drawing.Font("Calibri", 9.75F);
             this.cbxTemplate.FormattingEnabled = true;
             this.cbxTemplate.Location = new System.Drawing.Point(95, 54);
             this.cbxTemplate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cbxTemplate.Name = "cbxTemplate";
-            this.cbxTemplate.Size = new System.Drawing.Size(218, 23);
+            this.cbxTemplate.Size = new System.Drawing.Size(217, 23);
             this.cbxTemplate.TabIndex = 1;
+            this.cbxTemplate.SelectedValueChanged += new System.EventHandler(this.cbxTemplate_SelectedValueChanged);
             // 
             // btnSend
             // 
-            this.btnSend.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnSend.Location = new System.Drawing.Point(112, 96);
+            this.btnSend.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnSend.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnSend.Location = new System.Drawing.Point(112, 130);
             this.btnSend.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(100, 33);
@@ -71,28 +77,58 @@
             // 
             // tbxUsername
             // 
+            this.tbxUsername.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tbxUsername.BackColor = System.Drawing.Color.White;
             this.tbxUsername.Enabled = false;
-            this.tbxUsername.Font = new System.Drawing.Font("Roboto", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.tbxUsername.Font = new System.Drawing.Font("Roboto", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
             this.tbxUsername.Location = new System.Drawing.Point(12, 12);
             this.tbxUsername.Name = "tbxUsername";
             this.tbxUsername.ReadOnly = true;
-            this.tbxUsername.Size = new System.Drawing.Size(301, 23);
+            this.tbxUsername.Size = new System.Drawing.Size(300, 23);
             this.tbxUsername.TabIndex = 3;
             this.tbxUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cboxSubTemplate
+            // 
+            this.cboxSubTemplate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboxSubTemplate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxSubTemplate.Font = new System.Drawing.Font("Calibri", 9.75F);
+            this.cboxSubTemplate.FormattingEnabled = true;
+            this.cboxSubTemplate.Location = new System.Drawing.Point(95, 89);
+            this.cboxSubTemplate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cboxSubTemplate.Name = "cboxSubTemplate";
+            this.cboxSubTemplate.Size = new System.Drawing.Size(217, 23);
+            this.cboxSubTemplate.TabIndex = 4;
+            this.cboxSubTemplate.Visible = false;
+            this.cboxSubTemplate.VisibleChanged += new System.EventHandler(this.cboxSubTemplate_VisibleChanged);
+            // 
+            // label2
+            // 
+            this.labelSubTemplate.AutoSize = true;
+            this.labelSubTemplate.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Bold);
+            this.labelSubTemplate.ForeColor = System.Drawing.Color.White;
+            this.labelSubTemplate.Location = new System.Drawing.Point(8, 89);
+            this.labelSubTemplate.Name = "label2";
+            this.labelSubTemplate.Size = new System.Drawing.Size(76, 19);
+            this.labelSubTemplate.TabIndex = 5;
+            this.labelSubTemplate.Text = "Template:";
+            this.labelSubTemplate.Visible = false;
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(140)))), ((int)(((byte)(173)))));
-            this.ClientSize = new System.Drawing.Size(325, 142);
+            this.ClientSize = new System.Drawing.Size(324, 176);
+            this.Controls.Add(this.labelSubTemplate);
+            this.Controls.Add(this.cboxSubTemplate);
             this.Controls.Add(this.tbxUsername);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.cbxTemplate);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Font = new System.Drawing.Font("Roboto", 9.75F);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
@@ -110,5 +146,7 @@
         private System.Windows.Forms.ComboBox cbxTemplate;
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.TextBox tbxUsername;
+        private System.Windows.Forms.ComboBox cboxSubTemplate;
+        private System.Windows.Forms.Label labelSubTemplate;
     }
 }
--- mainForm.cs
+++ mainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Configuration;
 using System.Windows.Forms;
 
 namespace Email_Assist_Tool
@@ -13,12 +14,17 @@ namespace Email_Assist_Tool
             InitializeComponent();
         }
 
+        private const string folderIdentifier = "(var.)";
+        string initialPath = Environment.CurrentDirectory;
+        private string selectedFolder = "";
+
         private void mainForm_Load(object sender, EventArgs e)
         {
+            cbxTemplate.SelectedItem = "";
+            this.MinimumSize = new System.Drawing.Size(340, 175);
+
             string user = Environment.UserName;
             tbxUsername.Text = "Welcome " + user + "!";
-
-            string initialPath = Environment.CurrentDirectory;
 
             DirectoryInfo di = new DirectoryInfo(initialPath + @"\config\Templates");
             string dirTemplates = initialPath + @"\config\Templates";
@@ -29,9 +35,17 @@ namespace Email_Assist_Tool
                 return;
             }
 
+            string[] directories = Directory.GetDirectories(dirTemplates);
             FileInfo[] files = di.GetFiles("*.msg");
 
             List<string> list = new List<string>();
+
+            foreach (string directory in  directories)
+            {
+                string folderName = $"{Path.GetFileName(directory)} {folderIdentifier}";
+                list.Add(folderName);
+            }
+
             foreach (FileInfo file in files)
             {
                 string fileString = file.Name;
@@ -46,46 +60,142 @@ namespace Email_Assist_Tool
 
         private void btnSend_Click(object sender, EventArgs e)
         {
-            string initialPath = Environment.CurrentDirectory;
-            string templateSelected = cbxTemplate.Text;
-            string templatePath = initialPath + @"\config\Templates\" + templateSelected + ".msg";
-
-            string templateDir = initialPath + @"\config\Templates";
-            string trashDir = initialPath + @"\config\trash";
-
-            if (!File.Exists(templatePath))
+            if (cboxSubTemplate.Visible == false)
             {
-                MessageBox.Show("Email template does not exist.");
-                return;
+                string templateSelected = cbxTemplate.Text;
+                string templatePath = initialPath + @"\config\Templates\" + templateSelected + ".msg";
+
+                string templateDir = initialPath + @"\config\Templates";
+                string trashDir = initialPath + @"\config\trash";
+
+                if (!File.Exists(templatePath))
+                {
+                    MessageBox.Show("Email template does not exist.");
+                    return;
+                }
+                else
+                {
+                    if (!Directory.Exists(trashDir))
+                    {
+                        Directory.CreateDirectory(trashDir);
+                    }
+
+                    string fName = templateSelected + ".msg";
+                    string newName = templateSelected + "-Copy.msg";
+                    string copyEmailPath = trashDir + "\\" + newName;
+                    try
+                    {
+                        File.Copy(Path.Combine(templateDir, fName), Path.Combine(trashDir, newName), true);
+                        new Process
+                        {
+                            StartInfo = new ProcessStartInfo(copyEmailPath)
+                            {
+                                UseShellExecute = true
+                            }
+                        }.Start();
+                    }
+                    catch (Exception err)
+                    {
+                        MessageBox.Show(err.Message);
+                        throw;
+                    }
+                }
+            }
+
+            else
+            {
+                string templateSelectedSubTemplate = cboxSubTemplate.Text;
+                string templatePathSubTemplate = initialPath + $@"\config\Templates\{selectedFolder}\" + templateSelectedSubTemplate + ".msg";
+
+                string templateDirSubTemplate = initialPath + $@"\config\Templates\{selectedFolder}";
+                string trashDir = initialPath + @"\config\trash";
+
+                if (!File.Exists(templatePathSubTemplate))
+                {
+                    MessageBox.Show("Email template does not exist.");
+                    return;
+                }
+                else
+                {
+                    if (!Directory.Exists(trashDir))
+                    {
+                        Directory.CreateDirectory(trashDir);
+                    }
+
+                    string fNameSubTemplate = templateSelectedSubTemplate + ".msg";
+                    string newNameSubTemplate = templateSelectedSubTemplate + "-Copy.msg";
+                    string copyEmailPathSubTemplate = trashDir + "\\" + newNameSubTemplate;
+                    try
+                    {
+                        File.Copy(Path.Combine(templateDirSubTemplate, fNameSubTemplate), Path.Combine(trashDir, newNameSubTemplate), true);
+                        new Process
+                        {
+                            StartInfo = new ProcessStartInfo(copyEmailPathSubTemplate)
+                            {
+                                UseShellExecute = true
+                            }
+                        }.Start();
+                    }
+                    catch (Exception err)
+                    {
+                        MessageBox.Show(err.Message);
+                        throw;
+                    }
+                }
+            }
+            
+        }
+
+        private void cboxSubTemplate_VisibleChanged(object sender, EventArgs e)
+        {
+            if (cboxSubTemplate.Visible == true)
+            {
+                this.MinimumSize = new System.Drawing.Size(340, 215);
+                this.Size = new System.Drawing.Size(this.Size.Width, 215);
             }
             else
             {
-                if (!Directory.Exists(trashDir))
-                {
-                    Directory.CreateDirectory(trashDir);
-                }
+                this.MinimumSize = new System.Drawing.Size(340, 175);
+                this.Size = new System.Drawing.Size(this.Size.Width, 175);
+            } 
+        }
 
-                string fName = templateSelected + ".msg";
-                string newName = templateSelected + "-Copy.msg";
-                string copyEmailPath = trashDir + "\\" + newName;
-                try
+        private void cbxTemplate_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string selectedApp = cbxTemplate.Text;
+
+            if (selectedApp.Substring(Math.Max(0, selectedApp.Length - 6)) == folderIdentifier)
+            {
+                cboxSubTemplate.Visible = true;
+                labelSubTemplate.Visible = true;
+
+                selectedFolder = selectedApp.Substring(0, selectedApp.Length - 7);
+
+                cboxSubTemplate.DataSource = null;
+
+                DirectoryInfo diSubTemplate = new DirectoryInfo(initialPath + $@"\config\Templates\{selectedFolder}");
+
+                FileInfo[] filesSubTemplate = diSubTemplate.GetFiles("*.msg");
+
+                List<string> listSubTemplate = new List<string>();
+
+                foreach (FileInfo file in filesSubTemplate)
                 {
-                    File.Copy(Path.Combine(templateDir, fName), Path.Combine(trashDir, newName), true);
-                    new Process
-                    {
-                        StartInfo = new ProcessStartInfo(copyEmailPath)
-                        {
-                            UseShellExecute = true
-                        }
-                    }.Start();
+                    string fileStringSubTemplate = file.Name;
+                    string newstringSubTemplate = fileStringSubTemplate.Substring(0, fileStringSubTemplate.Length - 4);
+                    listSubTemplate.Add(newstringSubTemplate);
+
                 }
-                catch (Exception err)
-                {
-                    MessageBox.Show(err.Message);
-                    throw;
-                }
+                String[] strSubTemplate = listSubTemplate.ToArray();
+
+                cboxSubTemplate.DataSource = strSubTemplate;
             }
-
+            else 
+            {
+                cboxSubTemplate.Visible = false;
+                labelSubTemplate.Visible = false;
+            }
+            
         }
     }
 }
